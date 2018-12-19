a3f60a92d7a1a5d740234a00e4e38b6d40989001
Dimis <1391441+dimisdas@users.noreply.github.com>
2018-12-19 11:28:17 +0200

    Fixed bot mode 3

--- src/instabot.py
+++ src/instabot.py
@@ -915,7 +915,7 @@ class InstaBot:
                                                     (self.add_time(self.unfollow_delay)/2)
                 return #DB doesn't have enough followers yet
 
-            if self.bot_mode == 0:
+            if self.bot_mode == 0 or self.bot_mode == 3:
                 log_string = "Trying to unfollow #%i: " % (self.unfollow_counter + 1)
                 self.write_log(log_string)
                 self.auto_unfollow()
@@ -1089,6 +1089,116 @@ class InstaBot:
                 # we are not following this account, hence we unfollowed it, let's keep track
                 insert_unfollow_count(self, user_id=current_id)
 
+    def unfollow_recent_feed(self):
+	    
+        if len(self.media_on_feed) == 0:
+            self.get_media_id_recent_feed()
+	    
+        if len(self.media_on_feed) != 0 and self.is_follower_number < 5 and time.time() > self.next_iteration["Unfollow"] and self.unfollow_per_day != 0:
+            self.get_media_id_recent_feed()
+            chooser = random.randint(0, len(self.media_on_feed) - 1)
+            self.current_user = self.media_on_feed[chooser]["node"]["owner"]["username"]
+            self.current_id = self.media_on_feed[chooser]["node"]["owner"]["id"]
+            
+            current_user = self.current_user
+            current_id = self.current_id
+            
+            if self.login_status:
+                log_string = "Getting user info : %s" % current_user
+                self.write_log(log_string)
+            if self.login_status == 1:
+                url_tag = self.url_user_detail % (current_user)
+                try:
+                    r = self.s.get(url_tag)
+                    if r.text.find('The link you followed may be broken, or the page may have been removed.') != -1:
+                        log_string = "Looks like account was deleted, skipping : %s" % current_user
+                        self.write_log(log_string)
+                        insert_unfollow_count(self, user_id=current_id)
+                        time.sleep(3)
+                        return False
+                    all_data = json.loads(re.search('window._sharedData = (.*?);</script>', r.text, re.DOTALL).group(1))['entry_data']['ProfilePage'][0]
+
+                    user_info = all_data['graphql']['user']
+                    i = 0
+                    log_string = "Checking user info.."
+                    self.write_log(log_string)
+
+                    follows = user_info['edge_follow']['count']
+                    follower = user_info['edge_followed_by']['count']
+                    media = user_info['edge_owner_to_timeline_media']['count']
+                    follow_viewer = user_info['follows_viewer']
+                    followed_by_viewer = user_info[
+                        'followed_by_viewer']
+                    requested_by_viewer = user_info[
+                        'requested_by_viewer']
+                    has_requested_viewer = user_info[
+                        'has_requested_viewer']
+                    log_string = "Follower : %i" % (follower)
+                    self.write_log(log_string)
+                    log_string = "Following : %s" % (follows)
+                    self.write_log(log_string)
+                    log_string = "Media : %i" % (media)
+                    self.write_log(log_string)
+                    if follows == 0 or follower / follows > 2:
+                        self.is_selebgram = True
+                        self.is_fake_account = False
+                        self.write_log('   >>>This is probably Selebgram account')
+                    elif follower == 0 or follows / follower > 2:
+                        self.is_fake_account = True
+                        self.is_selebgram = False
+                        self.write_log('   >>>This is probably Fake account')
+                    else:
+                        self.is_selebgram = False
+                        self.is_fake_account = False
+                        self.write_log('   >>>This is a normal account')
+
+                    if media > 0 and follows / media < 25 and follower / media < 25:
+                        self.is_active_user = True
+                        self.write_log('   >>>This user is active')
+                    else:
+                        self.is_active_user = False
+                        self.write_log('   >>>This user is passive')
+
+                    if follow_viewer or has_requested_viewer:
+                        self.is_follower = True
+                        self.write_log("   >>>This account is following you")
+                    else:
+                        self.is_follower = False
+                        self.write_log('   >>>This account is NOT following you')
+
+                    if followed_by_viewer or requested_by_viewer:
+                        self.is_following = True
+                        self.write_log('   >>>You are following this account')
+
+                    else:
+                        self.is_following = False
+                        self.write_log('   >>>You are NOT following this account')
+
+                except:
+                    logging.exception("Except on auto_unfollow!")
+                    time.sleep(3)
+                    return False
+            else:
+                return False
+
+            if (
+                    self.is_selebgram is not False
+                    or self.is_fake_account is not False
+                    or self.is_active_user is not True
+                    or self.is_follower is not True
+            ):
+                self.write_log(current_user)
+                self.unfollow(current_id)
+                self.next_iteration["Unfollow"] = time.time() + self.add_time(self.unfollow_delay)
+                # don't insert unfollow count as it is done now inside unfollow()
+                #insert_unfollow_count(self, user_id=current_id)
+            elif self.is_following is not True:
+                # we are not following this account, hence we unfollowed it, let's keep track
+                insert_unfollow_count(self, user_id=current_id)
+        time.sleep(8)
+
+
+    
     def get_media_id_recent_feed(self):
         if self.login_status:
             now_time = datetime.datetime.now()
